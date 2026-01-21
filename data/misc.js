a7882ca2e98ea45a6d8f9de60c689968b36fb5f1
beancies <bcbeanie@protonmail.com>
2026-01-21 14:11:41 -0600

    Create misc.js
    Companions, Necromech, Vehicles, Hounds, Mods
    Syndicates

---
+++ data/misc.js
@@ -0,0 +1,15 @@
+export const miscItems = [
+  // Include Companions, Archwings, Necramechs, Vehicles, Hounds, etc.
+  { name: "ADARZA KAVAT", loc: "Genetics", cat: "Weapon" }, 
+  { name: "AMESHA", loc: "Tenno Lab (Dojo)", cat: "Weapon" }
+];
+
+export const SYNDICATES = [
+  { name: "The Hex", loc: "Höllvania Mall" },
+  { name: "Cavia", loc: "Sanctum Anatomica" },
+  { name: "The Holdfasts", loc: "Zariman" },
+  { name: "Solaris United", loc: "Fortuna" },
+  { name: "Ostron", loc: "Cetus" },
+  { name: "Vox Solaris", loc: "Little Duck" },
+  { name: "Entrati", loc: "Necralisk" }
+];
