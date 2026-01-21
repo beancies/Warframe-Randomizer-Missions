78dcecfb928c0a9be31897f4491563baf3754f4a
beancies <bcbeanie@protonmail.com>
2026-01-21 14:10:33 -0600

    Create weapons.js

---
+++ data/weapons.js
@@ -0,0 +1,7 @@
+export const weapons = [
+  { name: "ACCELTRA", loc: "Ur (Uranus) - Demolisher Drops", cat: "Weapon" },
+  { name: "AMPREX", loc: "Energy Lab (Dojo)", cat: "Weapon" },
+  // ... (Combine all Primary, Secondary, and Melee lists here)
+  // Ensure all categories are set to "Weapon" for the logic to function
+  { name: "ZENISTAR", loc: "Daily Login Reward", cat: "Weapon" }
+];
